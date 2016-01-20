d9b0b388b95fdf4001cd7d8ce4a40ead543980af
Mike <Michaelyao88@gmail.com>
2016-01-20 11:05:03 -0600

    do 4.3

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,27 @@
+puts "What's your first name?"
+firstname=gets.chomp
+puts "What's your middle name?"
+middlename=gets.chomp
+puts "What's your last name?"
+lastname=gets.chomp
+puts "Hello "+firstname+" "+middlename+" "+lastname
+
+#How do you define a local variable?
+#It's a variable that's specific to that method
+
+#How do you define a method?
+#It's like this thing that takes in variables
+
+
+#What is the difference between a local variable and a method?
+#Local variable can be changed with different values. Method takes in variables/values and does stuff
+
+#How do you run a ruby program from the command line?
+#ruby programname
+
+#How do you run an RSpec file from the command line?
+#rspec programname
+
+
+#What was confusing about this material? What made sense?
+#all made sense
