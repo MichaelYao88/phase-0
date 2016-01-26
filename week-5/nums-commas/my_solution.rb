bef790b3bcba6770cc6addd3c8051d78abad993f
Mike <Michaelyao88@gmail.com>
2016-01-26 15:26:21 -0600

    fix error

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -13,14 +13,29 @@
 # What are the steps needed to solve the problem?
 
 #convert it to string
-# mod3 the length
+#mod3 the length
 #if it's 1 or 2, put a comma after the first or second
-#looper pooper it using .insert on mod3+3 until it hits character length
+#I guess special case for 0 where you add 3?
+#loop it using .insert on every mod3+3 characters until it hits character length
+#return new number
 
 
 
 
 # 1. Initial Solution
+#def separate_comma(number)
+#  first_comma=number.to_s.length-3
+#  new_number=number.to_s
+#  while first_comma>0 do
+#    new_number=new_number.to_s.insert(first_comma,",")
+#    first_comma=first_comma-3
+#  end
+#  return new_number
+#end
+
+
+
+# 2. Refactored Solution
 def separate_comma(number)
   first_comma=number.to_s.length-3
   new_number=number.to_s
@@ -32,19 +47,15 @@ def separate_comma(number)
 end
 
 
-
-# 2. Refactored Solution
-
-
-#idk how to make my code shorter
+#idk how to make my code shorter, most of the documentation uses regexp
 
 # 3. Reflection
 #What was your process for breaking the problem down? What different approaches did you consider?
 #I tried to think about it logically but then when I coded it stuff stopped making sense so I tinkered a bit
 #Was your pseudocode effective in helping you build a successful initial solution?
-#sorta, it was actually wrong at the end
+#sorta, it was actually more complex than the solution I came up with at the end so I tossed it
 #What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
-#I looked through but could not find any new methods to help shorten my code
+#I looked through but could not find any new methods to help shorten my code (at least how I've written it)
 #How did you initially iterate through the data structure?
 #using a while loop
 #Do you feel your refactored solution is more readable than your initial solution? Why?
